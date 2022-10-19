fa24841ee7650b7e0d8d8a36017c9c7a7e7b9425
mulki2020 <malikadilib@gmail.com>
2022-10-19 12:02:38 +0300

    start

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>success doesn't come overnight </Text>
+      <Text>success doesn't come overnight </Text>
+      <Text>success doesn't come overnight </Text>
+ <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
