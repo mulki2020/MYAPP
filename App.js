175c3af302be98c3a33291d3435e77df0be60ef1
mulki2020 <malikadilib@gmail.com>
2022-10-19 12:25:18 +0300

    changes background color

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
